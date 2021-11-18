cb31cb66ffd11bbb7aefcaa868940a0579a85588
kgagracz <57999267+kgagracz@users.noreply.github.com>
2021-11-18 20:43:15 +0100

    Add files via upload

---
+++ PITurle/src/Turtle/TurtleAlgorithms.java
@@ -0,0 +1,71 @@
+package Turtle;
+
+
+import turtlePck.TurtleGraphicsWindow;
+
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ZTILabPI
+ */
+public class TurtleAlgorithms extends TurtleGraphicsWindow{
+    
+    public void positioningTurtle(int angle){
+        right(angle);
+    }
+
+    public void zadB1 (int bok, int minBok) {
+        if (bok < minBok) return;
+        for (int i=0; i < 2; i++) {
+            zadB1(bok/2, minBok);
+            for(int j=0; j<2; j++) {
+                forward(bok);
+                left(90);
+            }
+        }
+    }
+
+
+    public void sierpinski(int bok, int minBok ){
+         if (bok<minBok) {
+            return;
+        }
+          for(int i=1;i<4;i++){
+              sierpinski(bok/2, minBok);
+              forward(bok);
+              right(120);
+          }
+      }
+
+    
+  
+public void snowFlake(int n, int bok){
+    if (n==0){
+        forward(bok);
+        return;
+    }
+    snowFlake(n-1, bok/3);
+    right(60);
+    snowFlake(n-1, bok/3);
+    left(120);
+    snowFlake(n-1, bok/3);
+    right(60);
+    snowFlake(n-1, bok/3);
+
+}
+
+
+public void callSnowFlake(int n, int bok){
+    for(int i=1; i<4; i++){
+      snowFlake(n, bok);
+      left(120);
+    }
+}
+
+}
